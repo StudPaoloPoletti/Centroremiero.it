4b4630025f69177d4a18a368fed61a8631101313
Paolo Poletti <129394368+StudPaoloPoletti@users.noreply.github.com>
2023-06-06 08:50:37 +0200

    Add files via upload

---
+++ Pages/Foresteria.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CentroRemiero.it.Properties
+{
+    public class ForesteriaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CentroRemiero.it.Pages
+{
+        public class IndexModel : PageModel
+    {
+
+        public string testo;
+
+        private readonly int _pageIndex;
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            testo = "prova";
+        }
+    }
+}
